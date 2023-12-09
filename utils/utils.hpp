461ac392ad1f71917ccc580a86828e74464f1f36
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2023-12-09 21:32:38 +0100

    update utils

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -12,4 +12,29 @@ namespace edgs {
     std::cout << std::format("Run time: {}\n", duration);
   }
 
+  inline std::vector<std::string_view> split(std::string& str, const char sep) {
+    std::stringstream sstr{str};
+    std::string segment {};
+
+    std::vector<std::string_view> result;
+    while(std::getline(sstr, segment, sep)) 
+      result.push_back(segment);
+    return result;
+  }
+
+  /**
+   * @brief join a vector of strings to a single string
+   *
+   * @param v input vector
+   * @param del char value to insert between vector values, default to none
+   */
+  inline std::string join(const std::vector<std::string>& v, const char del = '\0') {
+    std::string s{};
+    for (const auto& part : v) { 
+      s += part;
+      if (&part != &*(v.end() - 1)) s += del;
+    }
+    return s;
+  }
+
 }
